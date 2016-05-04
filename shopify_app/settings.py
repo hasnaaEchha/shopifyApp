5b0f0ad11d30e3b81b448da1327a9d8b7f690208
hasnaaEchha <hasnaa.ech@gmail.com>
2016-05-04 22:02:05 +0100

    setting

--- shopify_app/settings.py
+++ shopify_app/settings.py
@@ -125,11 +125,11 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/public/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'public/static'),
 )
 
 # Simplified static file serving.
