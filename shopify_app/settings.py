5e3dede0fb7b611d786cf73bec8f698585f93056
hasnaaEchha <hasnaa.ech@gmail.com>
2016-05-05 00:05:12 +0100

    requirement

--- shopify_app/settings.py
+++ shopify_app/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 )
 
 ROOT_URLCONF = 'shopify_app.urls'
