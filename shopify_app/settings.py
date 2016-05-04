f06158e4cf257a1ac92b2cd91d58e638826756fb
hasnaaEchha <hasnaa.ech@gmail.com>
2016-05-04 21:16:21 +0100

    settings

--- shopify_app/settings.py
+++ shopify_app/settings.py
@@ -16,6 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '08il02y-z)9&f3f$c03%o8@@3$z0x^4i!_zm8*abt^1wc)!=*t'
 
+#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
